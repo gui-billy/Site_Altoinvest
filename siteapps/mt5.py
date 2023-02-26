7eb608097815132d97b743f6e20b55e70804e140
Guilherme Mendonca <guilhermeme@hotmail.com>
2023-02-26 15:25:26 -0400

    licença remota mt5

--- siteapps/mt5.py
+++ siteapps/mt5.py
@@ -19,13 +19,14 @@ def mt5(request):
             if (data_tuple[0][1] == item["broker"]) and (data_tuple[1][1] == item["account"]):
                 dtt = datetime.strptime(item["exp_date"], "%Y-%m-%d")
                 if (datetime.now() < dtt):
-                    return JsonResponse({"broker": data_tuple[0][1], "account": data_tuple[1][1], "exp_date": item["exp_date"]})  
+                    response_data = {"broker": data_tuple[0][1], "account": data_tuple[1][1], "exp_date": item["exp_date"]}
+                    return JsonResponse(response_data, status=200, json_dumps_params={'ensure_ascii': False}) 
                 else:
                     exp_date = datetime.strptime(item["exp_date"], "%Y-%m-%d").strftime("%d-%m-%Y")
                     response_data = {"ERRO": 'Licença Expirada', "Data": exp_date}
                     return JsonResponse(response_data, json_dumps_params={'ensure_ascii': False})
         response_data = {"ERRO": 'Conta Inválida'}
-        return JsonResponse(response_data, json_dumps_params={'ensure_ascii': False})
+        return JsonResponse(response_data, status=200, json_dumps_params={'ensure_ascii': False})
     
     response_data = {"ERRO": 'Método Inválido'}
     return JsonResponse(response_data, status=400, json_dumps_params={'ensure_ascii': False})
